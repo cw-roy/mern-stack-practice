08187060b9d8dab98a1149bbb5eaeef62bd098bd
cw-roy <curtis_roy@msn.com>
2023-08-14 17:54:15 -0700

    Updated server index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // Move to env
-const CONNECTION_URL = 'mongodb+srv://cluster1user:YBUVQrTh0AxRTLoF@cluster1.46jnohf.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'nunyabidness'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
