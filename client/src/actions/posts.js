c746e6cc37d2a35a4a45fa24a07387feb61396f1
cw-roy <curtis_roy@msn.com>
2023-08-19 07:36:34 -0700

    Clean up commented-out code

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -51,36 +51,3 @@ export const deletePost = (id) => async (dispatch) => {
     console.log(error.message);
   }
 };
-
-
-// import * as api from '../api/index.js';
-
-// export const getPosts = () => async (dispatch) => {
-//     try {
-//         const { data } = await api.fetchPosts();
-
-//         dispatch({ type: 'FETCH_ALL', payload: data });
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
-
-// export const createPost = (post) => async (dispatch) => {
-//     try {
-//         const { data } = await api.createPost(post);
-
-//         dispatch({ type: 'CREATE', payload: data });
-//     } catch (error) {
-//         console.lo(error);
-//     }
-// }
-
-// export const updatePost = (id, post) => async (dispatch) => {
-//     try {
-//         const { data } = await api.updatePost(id, post);
-
-//         dispatch({ type: 'UPDATE', payload: data });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -12,15 +12,3 @@ router.delete('/:id', deletePost);
 router.patch('/:id/likePost', likePost);
 
 export default router;
-
-// import express from "express";
-
-// import { getPosts, createPost, updatePost } from "../controllers/posts.js";
-
-// const router = express.Router();
-
-// router.get('/', getPosts);
-// router.post('/', createPost);
-// router.patch('/:id', updatePost)
-
-// export default router;
